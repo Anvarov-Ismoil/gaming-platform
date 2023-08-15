14ce302598622e66f22b40ceccd4d49212d04155
Islom <islomk758@gmail.com>
2023-08-15 20:12:37 +0500

    new

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,37 @@
 <template>
   <main>
-    <div class="home">
-      <div class="titles w-[527px] flex flex-col pt-[220px] pl-[211px] ">
+    <div class="hero flex flex-row justify-between">
+      <div class="titles w-[527px] flex flex-col pt-[210px] pl-[211px] ">
         <h3 class="text-xl text-[#E87D0E]">3D game Dev</h3>
-        <h1>Work that we produce for our clients</h1>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.</p>
-        <button class="btn bg-brand-orange px-8 py-3 rounded-full">Get more details</button>
+        <h1 class="text-[50px]">Work that we produce for our clients</h1>
+        <p class="text-[16px] pt-[26px] pb-[50px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.</p>
+        <button class="w-[195px] h-[53px] bg-[#FA8305] rounded-[40px] text-[16px] px-[31px] py-[14px]">Get more details</button>
+      </div>
+      <img src="../image/Main.png" alt="#" class="img pr-[25px] w-[739px] h-[656px] pt-[126px]">
+    </div>
+
+    <div class="image pt-32">
+      <div class="title gap-[500px] flex justify-around items-center">
+        <h1 class="text-[31px]">Currently Trending Games</h1>
+        <button class="btn bg-gray-700 py-[10px] px-[30px]">SEE ALL</button>
+      </div>
+      <div class="cards flex justify-center flex-row gap-[47px]">
+        <div class="image-block pt-14 w-[250px]">
+          <img src="../image/Home.png" class="w-[250px] h-[277px]" alt="#">
+          <p class="pt-6"><i class="fab fa-gripfire text-center"></i> 40 Followers</p>
+        </div>
+        <div class="image-block pt-14 w-[250px]">
+          <img src="../image/Home1.png" class="w-[250px] h-[277px]" alt="#">
+          <p class="pt-6"><i class="fab fa-gripfire text-center"></i> 40 Followers</p>
+        </div>
+        <div class="image-block pt-14 w-[250px]">
+          <img src="../image/Home2.png" class="w-[250px] h-[277px]" alt="#">
+          <p class="pt-6"><i class="fab fa-gripfire text-center"></i> 40 Followers</p>
+        </div>
+        <div class="image-block pt-14 w-[250px]">
+          <img src="../image/Main.png" class="w-[250px] h-[277px]" alt="#">
+          <p class="pt-6"><i class="fab fa-gripfire text-center"></i> 40 Followers</p>
+        </div>
       </div>
     </div>
   </main>
