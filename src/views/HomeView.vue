eed89913ffbf3652bc2e2838e631421fff6ca5b7
Islom <islomk758@gmail.com>
2023-08-15 19:24:52 +0500

    4 commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <template>
   <main>
-    <h1 class="text-center font-bold text-red-500">Hello world</h1>
-    <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, saepe!</h2>
+    <div class="home">
+      <div class="titles w-[527px] flex flex-col pt-[220px] pl-[211px] ">
+        <h3 class="text-xl text-[#E87D0E]">3D game Dev</h3>
+        <h1>Work that we produce for our clients</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.</p>
+        <button class="btn bg-brand-orange px-8 py-3 rounded-full">Get more details</button>
+      </div>
+    </div>
   </main>
 </template>
 
